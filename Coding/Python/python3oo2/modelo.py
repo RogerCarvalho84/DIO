56047edf101d0e40db5189350bc13b7646a9040b
RogerCarvalho84 <roger.carvalho984@gmail.com>
2021-10-03 17:19:27 -0300

    Adicionando projeto python3oo2

---
+++ Coding/Python/python3oo2/modelo.py
@@ -0,0 +1,48 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes +=1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self,novo_nome):
+        self._nome = novo_nome.title()
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super.__init__(nome, ano)
+        self.duracao = duracao
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super.__init__(nome, ano)
+        self.temporadas = temporadas
+
+
+
+vingadores = Filme("vingadores - guerra Infinita", 2018, 160)
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - '
+      f'Duração: {vingadores.duracao} mins - Likes: {vingadores.likes}')
+
+
+
+atlanta = Serie("atlanta", 2018, 2)
+atlanta.dar_like()
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - '
+      f'Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
