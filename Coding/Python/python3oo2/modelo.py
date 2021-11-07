0a99a409860f2186afe1165c26396b0c32b0594e
RogerCarvalho84 <roger.carvalho984@gmail.com>
2021-11-06 23:51:27 -0300

    Informações de URL

--- Coding/Python/python3oo2/modelo.py
+++ Coding/Python/python3oo2/modelo.py
@@ -4,7 +4,6 @@ class Programa:
         self.ano = ano
         self._likes = 0
 
-
     @property
     def likes(self):
         return self._likes
@@ -20,29 +19,63 @@ class Programa:
     def nome(self,novo_nome):
         self._nome = novo_nome.title()
 
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self._likes} likes')
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        super.__init__(nome, ano)
+        super(Filme, self).__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        super.__init__(nome, ano)
+        super(Serie, self).__init__(nome, ano)
         self.temporadas = temporadas
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} likes'
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __len__(self):
+        return len(self._programas)
 
 
 
 vingadores = Filme("vingadores - guerra Infinita", 2018, 160)
-vingadores.dar_like()
-vingadores.dar_like()
-vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - '
-      f'Duração: {vingadores.duracao} mins - Likes: {vingadores.likes}')
-
-
-
 atlanta = Serie("atlanta", 2018, 2)
+tmep = Filme("todo mundo em pânico", 1999, 100)
+demolidor = Serie("demolidor", 2016, 2)
+
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+demolidor.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
 atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - '
-      f'Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist("fim de semana", filmes_e_series)
+
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}\n')
+print(playlist_fim_de_semana[0])
+
+for programa in playlist_fim_de_semana:
+    print(programa)
