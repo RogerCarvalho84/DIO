70b78e3c35943ee69c8704cf1e772029e7c81043
RogerCarvalho84 <roger.carvalho984@gmail.com>
2021-11-18 23:02:40 -0300

    Python - Validação de dados

--- Coding/Python/python_brasilidades/main.py
+++ Coding/Python/python_brasilidades/main.py
@@ -1,6 +1,26 @@
-from validate_docbr import CPF
-from cpf import Cpf
+from cpf_cnpj import Documento
+from validate_docbr import CNPJ
+from telefones_br import TelefonesBr
+import re
 
+"""
+exemplo_cnpj = "35379838000112"
+exemplo_cpf = "32405754851"
+documento = Documento.cria_documento(exemplo_cpf)
 
-cpf = CPF()
-print(cpf.validate("324057454851"))
+print(documento)
+"""
+
+texto = "meu telefone é 13981732740 ou 1332514425"
+
+telefone = "551332514425"
+telefone_objeto = TelefonesBr(telefone)
+
+#print(telefone_objeto)
+
+#padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+
+#resposta = re.search(padrao,telefone)
+#print(resposta.group(2))
+
+print(telefone_objeto)
